06982aa8ee509f42d21882ccad4f7e83ab02958c
Emiliano <es.hotes@gmail.com>
2020-06-28 14:54:01 -0300

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,38 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { CheckoutComponent } from "./components/checkout/checkout.component";
+import { ProductoSingleComponent } from "./components/producto-single/producto-single.component";
+import { CategoriasComponent } from "./components/categorias/categorias.component";
+import { ProductosComponent } from "./components/productos/productos.component";
+import { InicioComponent } from "./components/inicio/inicio.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: InicioComponent,
+  },
+  {
+    path: "productos",
+    component: ProductosComponent,
+  },
+  {
+    path: "categorias",
+    component: CategoriasComponent,
+  },
+  {
+    path: "producto_single",
+    component: ProductoSingleComponent,
+  },
+  {
+    path: "checkout",
+    component: CheckoutComponent,
+  },
+  // Default redirection
+  { path: "**", redirectTo: "" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
